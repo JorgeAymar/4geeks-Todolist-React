2cd16e7935996f8e081573e951b7a7f2f2a8eec1
Jorge Sánchez Aymar <jorge.s.aymar@gmail.com>
2023-03-24 22:59:57 +0000

    update taller

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [tarea, setTareas] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+
+	const handleAddTask = () => {
+		  setTareas([...tarea, inputValue]);
+		  setInputValue('');
+	};
+
+	const handleInputChange = (event) => {
+		setInputValue(event.target.value);
+	};  
+ 
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="container mt-5">
+				<div className="row justify-content-center">
+					<div className="col-lg-6 col-md-8 col-sm-10">
+						<h1 className="text-center mb-5">Todo List</h1>
+
+						<div className="form-group bg-info p-3">
+							<input
+							className="form-control mb-3"
+							type="text"
+							id="todo-input"
+							value={inputValue}
+							onChange={handleInputChange}
+							name="text"
+							autoComplete="off"
+							placeholder="Ingrese una tarea"
+							/>
+							<button
+							className="btn btn-outline-secondary"
+							type="button"
+							onClick={handleAddTask}
+							>
+							Agregar tarea
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row mx-auto" style={{width: "640px"}}>
+					<ul className="list-group">
+					{tarea.map((elem, index) => (
+					<li className="list-group-item"> {elem} </li>
+					))}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
